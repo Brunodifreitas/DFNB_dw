b3419dec2d7f030075856307f06f61b231c3e686
Brunodifreitas <59854129+Brunodifreitas@users.noreply.github.com>
2020-04-08 00:08:56 -0600

    IT 243 - Added LoadNAICSCodeHierDim_bf.dtsx

---
+++ stored_procedures/dbo.usp_LoadNAICSCodeHier.sql
@@ -0,0 +1,30 @@
+--EXEC	[dbo].[usp_LoadNAICSCodeHier]
+
+--SELECT	*
+--FROM	[dbo].[tblNAICSCodeHier]
+
+
+
+USE [DFNB3];
+GO
+
+
+DROP PROCEDURE dbo.usp_LoadNAICSCodeHier;
+GO
+
+
+SET ANSI_NULLS ON;
+GO
+
+SET QUOTED_IDENTIFIER ON;
+GO
+
+CREATE PROCEDURE dbo.usp_LoadNAICSCodeHier
+AS
+
+BEGIN
+TRUNCATE TABLE	[dbo].[tblNAICSCodeHier]
+INSERT	INTO	[dbo].[tblNAICSCodeHier]
+SELECT	v.*
+FROM [etl].[v_naics_code_hier_load] AS v
+END;
